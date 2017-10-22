323c7c8ed60ac57053890558eed90285275f3a9b
DESKTOP-OIO0DRA\nnnyy <nnnyyy@nate.com>
2017-10-22 12:59:04 +0900

    애니메이션 작업 추가

--- damoney/app/src/main/java/com/dacom/damoney/BMHomeFragment.java
+++ damoney/app/src/main/java/com/dacom/damoney/BMHomeFragment.java
@@ -3,13 +3,20 @@ package com.dacom.damoney;
 
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.widget.Toast;
 
+import com.daasuu.library.DisplayObject;
+import com.daasuu.library.drawer.SpriteSheetDrawer;
+import com.daasuu.library.easing.Ease;
+import com.daasuu.library.util.Util;
 import com.dacom.damoney.Advertisement.AdsManager;
 import com.dacom.damoney.Advertisement.AdsResultListener;
 import com.dacom.damoney.databinding.FragmentBmhomeBinding;
@@ -33,7 +40,20 @@ public class BMHomeFragment extends Fragment implements AdsResultListener{
         mBind = DataBindingUtil.inflate(inflater, R.layout.fragment_bmhome, container, false);
         setupAds();
         setupButtonEvent();
+        setupAnim();
         return mBind.getRoot();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mBind.animationTextureView.tickStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mBind.animationTextureView.tickStop();
     }
 
     private void setupAds() {
@@ -59,6 +79,49 @@ public class BMHomeFragment extends Fragment implements AdsResultListener{
         });
     }
 
+    private void setupAnim() {
+
+        mBind.animationTextureView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                mBind.animationTextureView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                int width  = mBind.animationTextureView.getMeasuredWidth();
+                int height  = mBind.animationTextureView.getMeasuredHeight();
+
+                Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.character_idle);
+                Bitmap bitmap2 = Bitmap.createScaledBitmap(bitmap,
+                        (int)Util.convertDpToPixel(480f,getContext()),
+                        (int)Util.convertDpToPixel(154f,getContext()),
+                        false
+                        );
+                final float frameWidth = Util.convertDpToPixel(160f, getContext());
+                final float frameHeight = Util.convertDpToPixel(154f, getContext());
+
+                SpriteSheetDrawer spriteSheetDrawer = new SpriteSheetDrawer(
+                        bitmap2,
+                        frameWidth,
+                        frameHeight, 3, 3)
+                        .frequency(10)
+                        .spriteLoop(true);
+
+                DisplayObject bitmapDisplay = new DisplayObject();
+                bitmapDisplay
+                        .with(spriteSheetDrawer)
+                        .tween()
+                        .tweenLoop(true)
+                        .transform((width - frameWidth)/2 - 50, (height - frameHeight)/2)
+                        .toX(500, (width - frameWidth)/2 + 50, Ease.CUBIC_IN)
+                        .toX(500, (width - frameWidth)/2 - 50, Ease.CUBIC_IN)
+                        .end();
+                mBind.animationTextureView
+                        .addChild(bitmapDisplay)
+                        .tickStart();
+            }
+        });
+
+
+    }
+
     @Override
     public void onAdsLoaded() {
         mBind.btnShowAds.setEnabled(true);
@@ -66,6 +129,7 @@ public class BMHomeFragment extends Fragment implements AdsResultListener{
 
     @Override
     public void onAdsFinished(int nRet) {
+        setupAnim();
         if(nRet == -1) {
             Toast.makeText(getContext(), "광고 리워드 받기에 실패 했습니다.", Toast.LENGTH_LONG).show();
             mBind.btnShowAds.setEnabled(false);
