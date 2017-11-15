a0ced17d4e81be1149cc79d9beb94aba8d34c9e2
WIN-00FA24355NA\Administrator <nnnyyy@nate.com>
2017-11-15 21:50:05 +0900

    createAccount

--- Databases/sp/CreateAcount.sql
+++ Databases/sp/CreateAcount.sql
@@ -3,5 +3,6 @@ DELIMITER $$
 CREATE procedure CreateAccount(_id varchar(20), _pwd varchar(20), _nick varchar(20), _status int)
 BEGIN
 	insert into account (id, password, nick, status) values (_id, password(_pwd), _nick, _status);
+	insert into point ( id, point ) values (_id, 0 );
 END$$
 DELIMITER ;
