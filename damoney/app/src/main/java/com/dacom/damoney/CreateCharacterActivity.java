7687ae47e12ae0bfb5f4f25e0326e47a3321a900
NEXON\nnnyyy <nnnyyy@nate.com>
2017-11-23 15:54:29 +0900

    캐릭터 생성

---
+++ damoney/app/src/main/java/com/dacom/damoney/CreateCharacterActivity.java
@@ -0,0 +1,13 @@
+package com.dacom.damoney;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateCharacterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_character);
+    }
+}
