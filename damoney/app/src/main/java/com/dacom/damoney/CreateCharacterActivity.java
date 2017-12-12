ce1ab8a71dd529b634765d0baedd0e2c482cf98d
DESKTOP-OIO0DRA\nnnyy <nnnyyy@nate.com>
2017-12-13 02:02:17 +0900

    이것저것 처리

--- damoney/app/src/main/java/com/dacom/damoney/CreateCharacterActivity.java
+++ damoney/app/src/main/java/com/dacom/damoney/CreateCharacterActivity.java
@@ -9,10 +9,26 @@ import android.view.View;
 import com.dacom.damoney.Sign.SigninActivity;
 import com.dacom.damoney.databinding.ActivityCreateCharacterBinding;
 
+import java.util.ArrayList;
+
 public class CreateCharacterActivity extends AppCompatActivity {
     ActivityCreateCharacterBinding mBind;
     String id;
     String pw;
+
+    ArrayList<CharacterInfoActivity.CharInfo> aCharacters = new ArrayList<>();
+    int selectedIndex = 0;
+
+    public static class CharInfo {
+        public CharInfo(int _resid, boolean _open) {
+            res_id = _resid;
+            bOpen = _open;
+        }
+
+        public int res_id;
+        public boolean bOpen;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +44,47 @@ public class CreateCharacterActivity extends AppCompatActivity {
                 requestSignup();
             }
         });
+
+        init();
+        setupButtonEvent();
+    }
+
+    protected void init() {
+        aCharacters.add(new CharacterInfoActivity.CharInfo(R.drawable.login_char, true));
+        aCharacters.add(new CharacterInfoActivity.CharInfo(R.drawable.cc_hidden_1, false));
+        aCharacters.add(new CharacterInfoActivity.CharInfo(R.drawable.cc_hidden_2, false));
+        aCharacters.add(new CharacterInfoActivity.CharInfo(R.drawable.cc_hidden_3, false));
+        mBind.ivCharacter.setImageResource(aCharacters.get(selectedIndex).res_id);
+    }
+
+    protected void setupButtonEvent() {
+        mBind.btnLeft.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveCharacterImage(true);
+            }
+        });
+
+        mBind.btnRight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveCharacterImage(false);
+            }
+        });
+    }
+
+    protected void moveCharacterImage(boolean isLeft) {
+        if( isLeft ) {
+            selectedIndex--;
+            if(selectedIndex < 0 ) selectedIndex = aCharacters.size() - 1;
+        }
+        else {
+            selectedIndex++;
+            if(selectedIndex >= aCharacters.size() ) selectedIndex = 0;
+        }
+
+        mBind.ivCharacter.setImageResource(aCharacters.get(selectedIndex).res_id);
+        //mBind.btnExit.setEnabled(aCharacters.get(selectedIndex).bOpen);
     }
 
     protected void requestSignup() {
